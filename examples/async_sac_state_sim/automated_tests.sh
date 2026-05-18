dd7a0a061914fdd83304201cf9e7d04e3e0cbcd4
ryanvanderstelt <ryan@vstelt.com>
2026-05-17 22:48:40 -0600

    edited test script and made capacity calculation automatic

--- examples/async_sac_state_sim/automated_tests.sh
+++ examples/async_sac_state_sim/automated_tests.sh
@@ -5,14 +5,15 @@ WANDB_OUTPUT_DIR=~/wandb_logs
 TEST="async_sac_state_sim.py"
 CONDA_ENV="serl"
 ENV="PandaReachCube-v0"
-MAX_STEPS=50000
+MAX_STEPS=10000
 TRAINING_STARTS=1000
 RANDOM_STEPS=1000
+BATCH_SIZE=256
 CRITIC_ACTOR_RATIO=8
-EXP_NAME="$ENV-FOR-PAPER"
+EXP_NAME="DISASSOCIATED_FRACTAL_TEST"
 REPLAY_BUFFER_TYPE="fractal_symmetry_replay_buffer"
 
-BASE_ARGS="--env $ENV --exp_name $EXP_NAME --wandb_output_dir $WANDB_OUTPUT_DIR --training_starts $TRAINING_STARTS --random_steps $RANDOM_STEPS"
+BASE_ARGS="--env $ENV --exp_name $EXP_NAME --wandb_output_dir $WANDB_OUTPUT_DIR --training_starts $TRAINING_STARTS --random_steps $RANDOM_STEPS --critic_actor_ratio $CRITIC_ACTOR_RATIO --batch_size $BATCH_SIZE"
 ARGS=""
 
 function run_test {
@@ -42,30 +43,45 @@ function run_test {
 }
 
 # BASELINE TESTING
-for CRITIC_ACTOR_RATIO in 8
+
+for replay_buffer_capacity in 5000
 do
-    for batch_size in 256
+    ARGS="--run_name baseline --replay_buffer_type replay_buffer --replay_buffer_capacity $replay_buffer_capacity"
+    run_test
+done
+
+# CONSTANT TESTING
+
+for starting_branch_count in 27
+do
+    for workspace_width in 0.5
     do
         for replay_buffer_capacity in 5000
         do
-            ARGS="--run_name baseline --critic_actor_ratio $CRITIC_ACTOR_RATIO --replay_buffer_type replay_buffer --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity"
+            ARGS="--run_name constant-$starting_branch_count*$starting_branch_count --replay_buffer_type $REPLAY_BUFFER_TYPE --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'constant' --starting_branch_count $starting_branch_count"
             run_test
         done
     done
 done
+ 
 
-# CONSTANT TESTING
-for CRITIC_ACTOR_RATIO in 8
+# FRACTAL TESTING
+for replay_buffer_capacity in 5000
 do
-    for starting_branch_count in 27
+    for workspace_width in 0.5
     do
-        for batch_size in 256
+        for alpha in 0.1 0.5 0.9
         do
-            for workspace_width in 0.4
+            for branching_factor in 3 9
             do
-                for replay_buffer_capacity in $((5000 * $starting_branch_count * $starting_branch_count)) $((1000000 * $starting_branch_count * $starting_branch_count))
+                for max_depth in 2 4
                 do
-                    ARGS="--run_name constant-$starting_branch_count^1 --critic_actor_ratio $CRITIC_ACTOR_RATIO --replay_buffer_type $REPLAY_BUFFER_TYPE --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'constant' --starting_branch_count $starting_branch_count"
+                    # Fractal Expansion
+                    ARGS="--run_name fractal_expansion-$branching_factor^$max_depth --replay_buffer_type $REPLAY_BUFFER_TYPE --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'fractal' --alpha $alpha --branching_factor $branching_factor --max_depth $max_depth"
+                    run_test
+
+                    # Fractal Contraction
+                    ARGS="--run_name fractal_contraction-$branching_factor^$max_depth --replay_buffer_type $REPLAY_BUFFER_TYPE --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'contraction' --alpha $alpha --branching_factor $branching_factor --max_depth $max_depth"
                     run_test
                 done
             done
@@ -73,58 +89,31 @@ do
     done
 done
 
-# # FRACTAL TESTING
-# for batch_size in 256
-# do
-#     for replay_buffer_capacity in 1000000
-#     do
-#         for workspace_width in 0.5
-#         do
-#             for alpha in 0.9
-#             do
-#                 for branching_factor in 3 9
-#                 do
-#                     for max_depth in 2 4
-#                     do
-#                         # Fractal Expansion
-#                         ARGS="--run_name fractal_expansion-$branching_factor^$max_depth-alpha-$alpha-workspace_width-$workspace_width-batch-size-$batch_size-capacity-$replay_buffer_capacity --replay_buffer_type $REPLAY_BUFFER_TYPE --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'fractal' --alpha $alpha --branching_factor $branching_factor --max_depth $max_depth"
-#                         run_test
 
-#                         # Fractal Contraction
-#                         ARGS="--run_name fractal_contraction-$branching_factor^$max_depth-alpha-$alpha-workspace_width-$workspace_width-batch-size-$batch_size-capacity-$replay_buffer_capacity --replay_buffer_type $REPLAY_BUFFER_TYPE --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'contraction' --alpha $alpha --branching_factor $branching_factor --max_depth $max_depth"
-#                         run_test
-#                     done
-#                 done
-#             done
-#         done
-#     done
-# done
+# DISASSOCIATIVE TESTING
 
-# # DISASSOCIATIVE TESTING
-# for batch_size in 256
-# do
-#     for replay_buffer_capacity in 1000000
-#     do
-#         for workspace_width in 0.5
-#         do
-#             for alpha in 0.9
-#             do
-#                 for min_branch_count in 1 3 9
-#                 do
-#                     for max_branch_count in 3 9 27
-#                     do
-#                         # Disassociative (Hourglass)
-#                         ARGS="--run_name disassociative-hourglass-$min_branch_count:$max_branch_count-alpha-$alpha-workspace_width-$workspace_width-batch-size-$batch_size-capacity-$replay_buffer_capacity --replay_buffer_type $REPLAY_BUFFER_TYPE --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'disassociated' --min_branch_count $min_branch_count --max_branch_count $max_branch_count --disassociated_type 'hourglass' --alpha $alpha"
-#                         run_test
+for replay_buffer_capacity in 5000
+do
+    for workspace_width in 0.5
+    do
+        for alpha in 0.1 0.5 0.9
+        do
+            for min_branch_count in 1 3 9
+            do
+                for max_branch_count in 3 9 27
+                do
+                    # Disassociative (Hourglass)
+                    ARGS="--run_name disassociative-hourglass-$min_branch_count:$max_branch_count --replay_buffer_type $REPLAY_BUFFER_TYPE --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'disassociated' --min_branch_count $min_branch_count --max_branch_count $max_branch_count --disassociated_type 'hourglass' --alpha $alpha"
+                    run_test
 
-#                         # Disassociative (Octahedron)
-#                         ARGS="--run_name disassociative-hourglass-$min_branch_count:$max_branch_count-alpha-$alpha-workspace_width-$workspace_width-batch-size-$batch_size-capacity-$replay_buffer_capacity --replay_buffer_type $REPLAY_BUFFER_TYPE --batch_size $batch_size --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'disassociated' --min_branch_count $min_branch_count --max_branch_count $max_branch_count --disassociated_type 'octahedron' --alpha $alpha"
+                    # Disassociative (Octahedron)
+                    ARGS="--run_name disassociative-octahedron-$min_branch_count:$max_branch_count --replay_buffer_type $REPLAY_BUFFER_TYPE --replay_buffer_capacity $replay_buffer_capacity --workspace_width $workspace_width --branch_method 'disassociated' --min_branch_count $min_branch_count --max_branch_count $max_branch_count --disassociated_type 'octahedron' --alpha $alpha"
 
-#                     done
-#                 done
-#             done
-#         done
-#     done
-# done
+                done
+            done
+        done
+    done
+done
+
 
 tmux kill-window -t serl_session:$SEED
