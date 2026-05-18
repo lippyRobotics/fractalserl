138e9cf38688043bb4e58b719fd9b830e02f3f29
Juan Rojas <rojas70@gmail.com>
2026-05-18 10:15:28 -0500

    fix memory allocation

--- examples/async_bin_relocation_fwbw_drq/run_actor.sh
+++ examples/async_bin_relocation_fwbw_drq/run_actor.sh
@@ -1,12 +1,13 @@
-export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
+export XLA_PYTHON_CLIENT_PREALLOCATE=true && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
 python async_drq_randomized.py "$@" \
     --actor \
     --render \
     --env FrankaBinRelocation-Vision-v0 \
-    --exp_name=serl_dev_drq_rlpd20demos_bin_fwbw_resnet_096 \
-    --seed 0 \
-    --random_steps 200 \
+    --exp_name=serl_drq_rlpd_20demos_objRel_fwbw \
+    --seed 1 \
+    --max_steps 30_000 \
+    --random_steps 0 \
     --encoder_type resnet-pretrained \
     --replay_buffer_type memory_efficient_replay_buffer \
     --branch_method "fractal" \
@@ -20,5 +21,3 @@ python async_drq_randomized.py "$@" \
     --bw_ckpt_path /home/student/code/serl/examples/async_bin_relocation_fwbw_drq/checkpoints/bw \
     --fw_reward_classifier_ckpt_path "/home/student/code/serl/examples/async_bin_relocation_fwbw_drq/classifier/fw_classifier_trained" \
     --bw_reward_classifier_ckpt_path "/home/student/code/serl/examples/async_bin_relocation_fwbw_drq/classifier/bw_classifier_trained" \
-    # --eval_checkpoint_step 31000 \
-    # --eval_checkpoint_step 1000
--- examples/async_bin_relocation_fwbw_drq/run_bw_learner.sh
+++ examples/async_bin_relocation_fwbw_drq/run_bw_learner.sh
@@ -1,5 +1,5 @@
-export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.45 && \
+export XLA_PYTHON_CLIENT_PREALLOCATE=true && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.4 && \
 export SCRIPT_DIR=$(dirname "$(realpath "$0")") && \
 export TIMESTAMP=$(date +"%m-%d-%Y-%H-%M-%S") && \
 export CHECKPOINT_DIR="$SCRIPT_DIR/checkpoints/bw-$TIMESTAMP" && \
@@ -8,32 +8,22 @@ python async_drq_randomized.py "$@" \
     --learner \
     --env FrankaBinRelocation-Vision-v0 \
     --exp_name=serl_dev_drq_rlpd20demos_bin_fwbw_resnet_096_bw \
-    --seed 0 \
+    --seed 1 \
+    --max_steps 30_000 \
     --random_steps 200 \
     --training_starts 200 \
     --critic_actor_ratio 4 \
     --batch_size 256 \
     --eval_period 2000 \
     --encoder_type resnet-pretrained \
-<<<<<<< Updated upstream
-    --replay_buffer_type fractal_symmetry_replay_buffer \
-    --replay_buffer_capacity 3_600_000 \
+    --replay_buffer_type memory_efficient_replay_buffer \
+    --replay_buffer_capacity 200_000 \
     --starting_branch_count 27 \
     --branch_method "constant" \
     --split_method "never" \
     --alpha 0.2 \
     --max_depth 3 \
     --branching_factor 3 \
-=======
-    --replay_buffer_type memory_efficient_replay_buffer \
-    --replay_buffer_capacity 3_600_000 \
-    --branch_method "fractal" \
-    --split_method "time" \
-    --branching_factor 3 \
-    --max_depth 3 \
-    --alpha 0.2 \
-    --max_traj_length 100 \
->>>>>>> Stashed changes
     --workspace_width 0.3 \
     --fwbw bw \
     --demo_path ./demos/bw_demos/baseline_01.pkl \
