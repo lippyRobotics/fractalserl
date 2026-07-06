b034d0c5b934554c10aafbfc8c7952427a7584b8
grceverett <>
2026-07-06 16:47:18 -0500

    fix default flags

--- examples/async_drq_sim/run_learner.sh
+++ examples/async_drq_sim/run_learner.sh
@@ -12,27 +12,23 @@ export TF_GPU_ALLOCATOR=cuda_malloc_async && \
 
 python "$SCRIPT_DIR/async_drq_sim.py" "$@"\
     --learner \
+    --alpha 0.2 \
+    --batch_size 256 \
+    --branch_method constant \
+    --checkpoint_path examples/async_drq_sim/checkpoints/pickcube-rlpd-classifier-sparse-demos \
+    --checkpoint_period 1000 \
+    --classifier_image_keys wrist \
+    --critic_actor_ratio 4 \
+    --demo_path examples/async_drq_sim/demos/pickcube_20_demos_classifier_sparse.pkl \
     --env PandaPickCubeVision-v0 \
     --exp_name SymmetryImplementationSimulation \
-    --run_name Training... \
-    --replay_buffer_type fractal_symmetry_replay_buffer \
-    --replay_buffer_capacity 30_000 \
     --max_steps 20_000 \
-    --training_starts 1000 \
     --random_steps 1000 \
-    --critic_actor_ratio 8 \
-    --batch_size 256 \
-    --starting_branch_count 9 \
-    --branch_method constant \
-    --workspace_width 0.15 \
-    --alpha 0.2 \
-    --demo_path examples/async_drq_sim/demos/pickcube_20_demos_classifier_sparse.pkl \
-    --checkpoint_period 1000 \
-    --checkpoint_path examples/async_drq_sim/checkpoints/pickcube-rlpd-classifier-sparse-demos \
-    --use_classifier_reward=True \
+    --replay_buffer_capacity 3_600_000 \
+    --replay_buffer_type memory_efficient_replay_buffer \
     --reward_classifier_ckpt_path examples/async_drq_sim/classifier/checkpoints/pickcube \
-    --reward_classifier_threshold=0.5 \
-    --classifier_image_keys wrist \
-    --classifier_use_proprio=False \
-    --zero_env_reward=True \
-    --terminate_on_classifier_success=True \
+    --run_name Training... \
+    --starting_branch_count 9 \
+    --training_starts 1000 \
+    --use_classifier_reward=True \
+    --workspace_width 0.15 \
